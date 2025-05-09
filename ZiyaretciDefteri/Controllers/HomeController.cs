005e7266f6b8278ae0e6f521d1575a44f4aed4e4
irmakcosar <cosarirmak8@gmail.com>
2025-05-09 16:43:22 +0300

    Add project files.

---
+++ ZiyaretciDefteri/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZiyaretciDefteri.Models;
+
+namespace ZiyaretciDefteri.Controllers
+{
+    public class HomeController : Controller
+    {
+        static List<MessageModel> messages = new List<MessageModel>();  
+        
+        public IActionResult Contact()
+        {
+
+            return View(messages);
+        }
+        [HttpPost]
+        public IActionResult Contact(MessageModel messagemodel)
+        {
+            //parametre adlarına dikkat et!!!!!
+                messages.Add(messagemodel);
+            
+
+                return RedirectToAction("Contact");
+            
+           
+        }
+    }
+}
---
+++ ZiyaretciDefteri/Models/MessageModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ZiyaretciDefteri.Models
+{
+    public class MessageModel
+    {
+        //kulanıcı bilgisi ve mesaj alma propetyleri
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
